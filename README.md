7f21a7c5a250a84c0af41f78161f13b9c8e48706
Patrick Kubiak <pk9948@rit.edu>
2021-10-09 16:59:41 -0400

    implement the basic algorithm

---
+++ README.md
@@ -0,0 +1,9 @@
+Install the matplotlib dependency. On linux this is usually something like:
+```
+pip3 install matplotlib
+```
+
+Run the code:
+```
+python3 main.py
+```
--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import matplotlib.pyplot as plt
+import random
 
 # 1.  Allocate an array of 100,000 elements,
 # one element to contain and track every square inch of the yard.
@@ -33,7 +34,44 @@ infested[0] = weeds
 # 	E.  For all samples that have been infested more then 14 days,
 # 	mark this sample of the lawn as ruined.
 
-'''
+# what is C?
+C = 1.1
+# infested = C ^ 14
+# I_tomorrow = R_0 * min(I_today * C, S_today)
+
+# TODO: what is R_0 ?
+r_naught = 0.9
+
+day = 1
+while weeds > 0 and grasses > 0 and day <= DURATION:
+    # A. exposed = C ^ 14
+    exposed = (int)(C ** 14)
+
+    # B. uninfested = grasses
+
+    # C.
+    new_exposures = min(exposed, grasses)
+
+    # D.
+    random_numbers = [random.uniform(0, 1) for i in range(new_exposures)]
+
+    # number of random numbers less than R_0
+    new_infested = len(list(filter(lambda n: n < r_naught, random_numbers)))
+
+    # update
+    weeds += new_infested
+    grasses -= new_infested
+
+    # record the day this square inch became infested
+    infested[day] = weeds
+
+    # increment day
+    day += 1
+
+# key,value pairs of day and number of weeds
+print(infested)
+
+"""
 while weeds > 0 and not grasses == 0:
     # A.
 
@@ -54,7 +92,7 @@ while weeds > 0 and not grasses == 0:
         infested[i] = infested[i - 1]
     infested[0] = new_exposures
     
-'''
+"""
 
 # 5.  Plot the resulting number of weed infestations per day.
 
